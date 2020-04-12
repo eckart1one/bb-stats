b8793c43d55debb5c459f807f8f27b0a9727ee6c
eckart invasor <eckart.inv3@gmail.com>
2020-04-12 09:20:04 -0500

    update react

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,14 @@ Route::get('/', function () {
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->middleware('verified');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
